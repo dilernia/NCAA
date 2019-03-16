a78c685cafcb505d6eddd33b35439817abbed9b5
Andrew DiLernia <andrew.s.dilernia@gmail.com>
2019-03-16 00:19:57 -0500

    Update Prediction_Methods.R
    (1) Removed knn
    (2) Added 2 more types of neural networks

--- Prediction_Methods.R
+++ Prediction_Methods.R
@@ -16,16 +16,17 @@ library(e1071)
 library(nnet)
 library(caret)
 library(kernlab)
-library(fastAdaboost)
+library(RSNNS)
 
 # Reading in data
 desResp <- readRDS("Our_Data/designResponse.rds")
 
 # Standardizing predictors and removing some columns
-# Aloc: location coding needs to be fixed
-designMat <- subset(desResp[[1]], select = -c(Awin, Season, Tourney, Spread))
-Aloc <- designMat$Aloc
-designMat <- as.data.frame(lapply(designMat, FUN = scale, center = TRUE, scale = TRUE))
+designMat <- subset(desResp[[1]], select = -c(Awin, Season, Tourney, Spread,
+                                              ATeamID, BTeamID, Aloc, Site))
+Aloc <- desResp[[1]]$Aloc
+designMat <- as.data.frame(lapply(designMat, FUN = scale, 
+                                  center = TRUE, scale = TRUE))
 
 # Adding location column back in
 designMat$Aloc <- as.factor(Aloc)
@@ -46,14 +47,11 @@ forms <- list(MOV = outcome ~ 0 + ., Awin =  outcome ~ 0 + .)
 designMats <- list(MOV = model.matrix(forms$MOV, data = modelDats$MOV),
                    Awin = model.matrix(forms$Awin, data = modelDats$Awin))
 
-# Estimating relationship between MOV and win prob
-SDmov <- sd(modelDats$MOV$outcome)
-
 # Setting parameters ------------------------------------------------------
 
 # Should be one of 'MOV' or 'Awin'
-#model <- "MOV"
-model <- "Awin"
+model <- "MOV"
+#model <- "Awin"
 
 # Setting training size and seed
 seed <- 1994
@@ -72,16 +70,15 @@ form <- forms[[model]]
 # Test Objects
 modelData.test <- modelDats[[model]][-trInds, ]
 designMat.test <- designMats[[model]][-trInds, ]
-outcome.test <- desResp[[2]][["Awin"]][-trInds]
 
 # Changing factor level names to work with train() function
 if(model == "Awin") {
-levels(modelData$outcome) <- c("loss", "win")
-ctrl <- trainControl(method = "cv", number = 5, classProbs =  TRUE)
-
-# Changing outcome for PCR to be numeric
-modelData.pcr <- modelData
-modelData.pcr$outcome <- as.integer(as.character(modelData$outcome) == "win")
+  levels(modelData$outcome) <- c("loss", "win")
+  ctrl <- trainControl(method = "cv", number = 5, classProbs =  TRUE)
+  
+  # Changing outcome for PCR to be numeric
+  modelData.pcr <- modelData
+  modelData.pcr$outcome <- as.integer(as.character(modelData$outcome) == "win")
 } else {
   ctrl <- trainControl(method = "cv", number = 5)
   modelData.pcr <- modelData
@@ -114,83 +111,117 @@ ldaFit <- lda(form, data = modelData)
 # (6) Optimally tuned mtry value random forest
 # For final run increase ntreeTry, decrease improve to 0.01
 defMtry <- floor(sqrt(ncol(modelData)))
-my.rf <- train(form = form, data = modelData, method = "rf", 
-                 trControl = ctrl, ntree = 1000,
-                 tuneGrid = expand.grid(mtry = floor(defMtry/2):(defMtry + 3)))
+my.rf <- caret::train(form = form, data = modelData, method = "rf", 
+                      trControl = ctrl, ntree = 1000,
+                      tuneGrid = expand.grid(mtry = floor(defMtry/2):(defMtry + 3)))
 
 # (7) Stochastic Gradient boosting
 # For final run try n.trees = 5000
-my.boost <- train(form = form, data = modelData, method = "gbm", 
-                  distribution = ifelse(model == "Awin", 
-                                        "bernoulli", "gaussian"),
-      trControl = ctrl, tuneGrid = expand.grid(n.trees = c(100), 
-                             shrinkage = c(0.01, seq(0.04, 0.16, by = 0.04)),
-                             interaction.depth = 1, n.minobsinnode = 10))
-
+my.boost <- caret::train(form = form, data = modelData, method = "gbm", 
+                         distribution = ifelse(model == "Awin", 
+                                               "bernoulli", "gaussian"),
+                         trControl = ctrl, tuneGrid = expand.grid(n.trees = c(50, 100, 150, 200, 300), 
+                                                                  shrinkage = c(0.01, seq(0.04, 0.16, by = 0.04)),
+                                                                  interaction.depth = 1, n.minobsinnode = 10))
 
 # (8) Artificial Neural Network w/ 1 hidden layer
-# For final run try tuneLength = 10, maxit = 5000
-my.nnet <- train(form, data = modelData, method = "nnet", 
-                        trControl = ctrl, trace = FALSE, maxit = 10,
-                        tuneGrid = expand.grid(size = c(5, 10, 15, 20),
-                                               decay = c(0.005, 0.01, 0.015)),
-                 linout = ifelse(model == "Awin", FALSE, TRUE))
+my.nnet <- caret::train(form, data = modelData, method = "nnet", 
+                        trControl = ctrl, trace = FALSE, maxit = 1000,
+                        tuneGrid = expand.grid(size = c(20),
+                                               decay = c(0.005, 0.01)),
+                        linout = ifelse(model == "Awin", FALSE, TRUE))
 
-# (9) Support Vector Machine (SVM)
+# (9) Artificial Neural Network w/ 3 hidden layers
+my.nnet3 <- caret::train(form, data = modelData, method = 'mlpWeightDecayML', 
+                  trControl = ctrl, trace = FALSE, maxit = 100,
+                  tuneGrid = expand.grid(layer1 = c(5, 15),
+                                         layer2 = c(0, 5, 15),
+                                         layer3 = c(0, 5, 15),
+                                         decay = c(0.01)),
+                  linout = ifelse(model == "Awin", FALSE, TRUE))
+
+# (10) Neural Network with Feature Extraction
+my.nnetf <- caret::train(form, data = modelData, method = 'pcaNNet', 
+                         trControl = ctrl, trace = FALSE, maxit = 5000,
+                         tuneGrid = expand.grid(size = c(20),
+                                                decay = c(0.01)),
+                         linout = ifelse(model == "Awin", FALSE, TRUE))
+
+# (11) Support Vector Machine (SVM)
 # For final run try kernal = c("sigmoid", "radial"), 
 # gamma = c(1/ncol(modelData), seq(0.005, 0.05, by = 0.005)), cost = seq(0.50, 2, by = 0.50)
-my.svm <- train(form, data = modelData, method = 'svmRadialCost', 
-      trControl = trainControl(method = "cv", number = 5,
-                               classProbs =  ifelse(model == "Awin", TRUE, FALSE)),
-      tuneGrid = expand.grid(C = c(0.1, 0.50, 1, 3, 5, 10)))
+my.svm <- caret::train(form, data = modelData, method = 'svmRadialCost', 
+                       trControl = ctrl,
+                       tuneGrid = expand.grid(C = c(0.1, 0.50, 1, 3, 5, 10)))
 
-
-# (10) k-Nearest Neighbour
-knnFit <- train(form, data = modelData,
-                method = "knn", trControl = ctrl, tuneLength = 20)
-
-# (11) AdaBoost Classification Tree
-my.ada <- train(form, data = modelData, method = 'adaboost', 
-                trControl = trainControl(method = "cv", number = 5,
-                                         classProbs =  ifelse(model == "Awin", TRUE, FALSE)))
-
-# Obtaining predicted probabilities
-treePreds <- predict(my.tree, modelData.test)
-prunedPreds <- predict(my.tree.pruned, modelData.test)
-rfPreds <- predict(my.rf, newdata = modelData.test, 
-                   type = ifelse(model == "Awin", "prob", "raw"))
-boostPreds <- predict(my.boost, modelData.test, 
-                          type = ifelse(model == "Awin", "prob", "raw"))
-nnetPreds <- predict(my.nnet, modelData.test, 
-                     type = ifelse(model == "Awin", "prob", "raw"))
-svmPreds <- predict(my.svm, modelData.test, 
-                     type = ifelse(model == "Awin", "prob", "raw"))
-cvglmPreds <- predict(cvglmFit, newx = designMat.test, 
-                      s = "lambda.min", type = "response")
-pcrPreds <- predict(pcrFit, newdata = modelData.test, ncomp = ncomps, type = "response")
-ldaPreds <- predict(ldaFit, newdata = modelData.test)$posterior
-knnPreds <- predict(knnFit, newdata = modelData.test,
-                    type = ifelse(model == "Awin", "prob", "raw"))
+# Function for obtaining predicted probabilities
+predCalc <- function(modelData = modelData.test, designMat = designMat.test) {
   
+  treePreds <- predict(my.tree, modelData)
+  prunedPreds <- predict(my.tree.pruned, modelData)
+  rfPreds <- predict(my.rf, newdata = modelData, 
+                     type = ifelse(model == "Awin", "prob", "raw"))
+  boostPreds <- predict(my.boost, modelData, 
+                        type = ifelse(model == "Awin", "prob", "raw"))
+  nnetPreds <- predict(my.nnet, modelData, 
+                       type = ifelse(model == "Awin", "prob", "raw"))
+  nnet3Preds <- predict(my.nnet3, modelData, 
+                        type = ifelse(model == "Awin", "prob", "raw"))
+  nnetfPreds <- predict(my.nnetf, modelData, 
+                        type = ifelse(model == "Awin", "prob", "raw"))
+  svmPreds <- predict(my.svm, modelData, 
+                      type = ifelse(model == "Awin", "prob", "raw"))
+  cvglmPreds <- predict(cvglmFit, newx = designMat, 
+                        s = "lambda.min", type = "response")
+  pcrPreds <- predict(pcrFit, newdata = modelData, ncomp = ncomps, type = "response")
+  ldaPreds <- predict(ldaFit, newdata = modelData)$posterior
+  
+  return(setNames(list(treePreds, prunedPreds, rfPreds, boostPreds, 
+                       nnetPreds, nnet3Preds, nnetfPreds, svmPreds, 
+                       cvglmPreds, pcrPreds, ldaPreds), 
+                  c("Full Tree", 'Pruned Tree', 
+                                    "Random Forest", 'Grad Boost', 
+                                    "NNet", "NNet3", "NNetF",
+                                    "SVM Radial",
+                                    "CV glmnet", "PCR", "LDA")))
+}
+
 # Preventing extrememe predictions. Need to tune thresh for final preds
 capper <- function(x, thresh = 0.01){
   if(is.null(dim(x)) == FALSE) {if(dim(x)[2] == 2) {x <- x[, 2]}}
   for(i in 1:length(x)) {
-  x[i] <- ifelse(x[i] < thresh, thresh, ifelse(x[i] > 1-thresh, 1-thresh, x[i]))
+    x[i] <- ifelse(x[i] < thresh, thresh, ifelse(x[i] > 1-thresh, 1-thresh, x[i]))
   }
   return(as.numeric(x))
 }
 
+if(model == "MOV") {
+  # Obtaining predicted probs for trainging data set
+  trPreds <- predCalc(modelData = modelData, designMat = designMat)
+  
+  # Estimating custom conversion between MOV and win probability using training data
+  # model fits
+  converts <- lapply(FUN = function(MOVhats) {
+    fit <- glm(formula = form, data = data.frame(outcome = desResp[[2]][["Awin"]][trInds], 
+                                                 movHat = MOVhats), family = "binomial")
+    return(fit)
+  }, X = trPreds)
+  
+  # Converting from MOV to estimated win prob
+  movList <- predCalc()
+  predList <- mapply(fit = converts, movs = movList, 
+                     FUN = function(fit, movs) {predict(fit, newdata = data.frame(movHat = movs), 
+                                                        type = 'response')}, SIMPLIFY = FALSE)
+} else {
+  predList <- predCalc()
+}
+
 # Capping predicted probabilities
-predList <- lapply(list(treePreds, prunedPreds, rfPreds, boostPreds, 
-                        nnetPreds, svmPreds, cvglmPreds, pcrPreds, 
-                        ldaPreds, knnPreds), FUN = capper)
-names(predList) <- c("Full Tree", 'Pruned Tree', "Random Forest",
-                     'Grad Boost', "NNet", "SVM Radial",
-                     "CV glmnet", "PCR", "LDA", "KNN")
+predList <- lapply(predList, FUN = capper)
 
 # Evaluating methods
-predSummary <- function(preds, response, truths = as.numeric(as.character(modelData.test$outcome))) {
+predSummary <- function(preds, response, 
+                        truths = as.numeric(as.character(modelDats[["Awin"]][-trInds, "outcome"]))) {
   
   misRate <- round(mean(abs(as.integer(preds > 0.50) - truths)), 4)
   meanSqErr <- round(mean((preds - truths)^2), 4)
@@ -205,19 +236,18 @@ predSummary <- function(preds, response, truths = as.numeric(as.character(modelD
                        Specificity = specificity,
                        `Log Loss` = round(logLoss, 4),
                        Response = ifelse(response == "Awin", "Win/Loss", "MOV"))
-  rownames(result) <- NULL
   return(result)
 }
 
 # Summarizing performance of methods
 results <- do.call("rbind", mapply(FUN = predSummary, preds = predList, 
-                            response = model, SIMPLIFY = FALSE))
+                                   response = model, SIMPLIFY = FALSE))
 
 # Saving results
 saveRDS(results, paste0("Runs/", model, "_Tr", trainSize, "_Seed", seed, ".rds"))
 
 # Available models fit using caret package:
-# http://topepo.github.io/caret/train-models-by-tag.html
+#http://topepo.github.io/caret/train-models-by-tag.html
 
 # Making a bracket from our predictions:
 #https://www.kaggle.com/c/mens-machine-learning-competition-2019/discussion/81642
